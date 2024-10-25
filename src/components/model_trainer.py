5e366426dc39171973c9473fd89be5d857b14de8
ivesh <iamvenkatesh14@gmail.com>
2024-10-25 21:27:39 +0530

    Grid search cv done model trained-0.8796, Randomsearchcv pending

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -38,8 +38,51 @@ class ModelTrainer:
                 "KNN Regressor":KNeighborsRegressor(),
                 "XGBoost Regressor":XGBRFRegressor()                
             }
+            params={
+                "Decision Tree Regressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting Regressor":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "KNN Regressor": {
+                    'n_neighbors': [3, 5, 7, 9, 11, 13],
+                    'weights': ['uniform', 'distance'],
+                    'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+                },
+                "XGBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256],
+                    'max_depth': [3, 4, 5, 6, 7, 8]
+                }
+                
+            }
             logging.info("Models are read, Now training models")
-            model_report:dict= evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            model_report:dict= evaluate_model(X_train=X_train,y_train=y_train,
+                                              X_test=X_test,y_test=y_test,models=models,params=params)
             logging.info("Model evaluation is done, Now fetching the score report for the best model")
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.metrics import r2_score
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV,RandomizedSearchCV
 
 from src.exception import CustomException
 
@@ -19,19 +19,30 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models,params):
     try:
         report = {}
         model_keys = list(models.keys())
-        model_values = list(models.values())
+        model_values = list(models.values())        
 
         for i in range(len(model_keys)):
             model = model_values[i]
             model_name = model_keys[i]
 
-            model.fit(X_train, y_train)
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+            # Perform hyperparameter tuning if parameters are specified for the model
+            if model_name in params and params[model_name]:
+                param_grid = params[model_name]
+                # Use GridSearchCV for exhaustive search, or RandomizedSearchCV for random search
+                grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+                grid_search.fit(X_train, y_train)
+                best_model = grid_search.best_estimator_
+            else:
+                best_model = model
+                best_model.fit(X_train, y_train)
+
+            # Train and evaluate the model
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
